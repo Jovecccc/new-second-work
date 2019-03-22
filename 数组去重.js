1033d1bb16960ef7ad21d73effe9794d9024a0cb
Jovecccc <44863701+Jovecccc@users.noreply.github.com>
2019-03-22 17:17:59 +0800

    Create 数组去重.js

---
+++ 数组去重.js
@@ -0,0 +1,39 @@
+
+// way 1
+var array = new Array(1,2,3,4,5,6,6,6,6,6);
+
+var index = function (array){
+    var array1 = new Array();
+    for(i=0;i<=array.length;i++){
+        if(array1.indexOf(array[i])===-1){
+            array1.push(array[i])
+        }
+    }
+    return array1;
+}
+
+// way 2
+var array = new Array(1,2,3,4,5,6,6,6,6,6);
+
+var index = function (array){
+    var set = new Set()
+    for(i=0;i<array.length;i++){
+        set.add(array[i]);
+    }
+    return set;
+}
+
+console.log(set);
+
+// way 3
+var array = new Array(1,2,3,4,5,6,6,6,6,6);
+
+var index = function (array){
+    var set = new Set()
+    for(i=0;i<array.length;i++){
+        if(set.has(array.length[i])==false){
+            set.add(array[i])
+        }
+    }
+    return set;
+}
